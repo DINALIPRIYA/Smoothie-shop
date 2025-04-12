3741d95d7c6fafdb6db89d2bc1dd146f5a3e3a9e
DINALIPRIYA <dinalipriyanjana@gmail.com>
2025-04-12 19:59:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(
 )
 
 #session = get_active_session()
-cnx=st.conncetion("snowflake")
+cnx=st.connection("snowflake")
 session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
